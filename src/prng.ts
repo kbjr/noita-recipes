c34400191f415709207c2e76a02c065eda2d9a5d
James Brumond <james@jbrumond.me>
2019-10-20 19:42:54 -0700

    typo

--- src/prng.ts
+++ src/prng.ts
@@ -4,7 +4,7 @@ const intMax = 0x7fffffff;
 // We need genuine int32 behavior when mutliplying (ie. accurate int32 overflow
 // behavior), so we're going to cheat and use an int32 array to have JS actually
 // treat it as int32 values. Defining this up here and reusing it to avoid having
-// to reallocate tiny 2 byte buffers over and over.
+// to reallocate tiny 8 byte buffers over and over.
 const a32 = new Int32Array(2);
 
 export class NumberGenerator {
